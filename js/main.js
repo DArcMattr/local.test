156b09dcdb750c32e111d5248414cc4073d2ea30
David Arceneaux <david@davidthemachine.org>
2020-08-02 14:14:10 -0500

    move function def out of IIFE

--- js/main.js
+++ js/main.js
@@ -1,39 +1,39 @@
 "use strict";
 
-( d => {
-	const timeInputFallback = inp => {
-		const inpMin = inp.getAttribute( "min" );
-		const inpMax = inp.getAttribute( "max" );
-		const baseDate = inpMin < inpMax ? "January 1, 1970" : "January 2, 1970";
+const timeInputFallback = inp => {
+	const inpMin = inp.getAttribute( "min" );
+	const inpMax = inp.getAttribute( "max" );
+	const baseDate = inpMin < inpMax ? "January 1, 1970" : "January 2, 1970";
 
-		const timeMin = new Date( `${baseDate } ${( inp.getAttribute( "min" ) )}` );
-		const timeMax = new Date( `${baseDate } ${( inp.getAttribute( "max" ) )}` );
-		const inpStep = parseInt( inp.getAttribute( "step" ) );
+	const timeMin = new Date( `${baseDate } ${( inp.getAttribute( "min" ) )}` );
+	const timeMax = new Date( `${baseDate } ${( inp.getAttribute( "max" ) )}` );
+	const inpStep = parseInt( inp.getAttribute( "step" ) );
 
-		const timeList = document.createElement( "datalist" );
-		const timeListId = `dl-${timeMin.getSeconds()}-${timeMax.getSeconds()}${inpStep}`;
+	const timeList = document.createElement( "datalist" );
+	const timeListId = `dl-${timeMin.getSeconds()}-${timeMax.getSeconds()}${inpStep}`;
 
-		if ( document.querySelector( `#${timeListId}` ) !== null ) {
-			return;
-		}
+	if ( document.querySelector( `#${timeListId}` ) !== null ) {
+		return;
+	}
 
-		timeList.setAttribute( "id", timeListId );
+	timeList.setAttribute( "id", timeListId );
 
-		let listOptions = "",
-			timeStep = timeMin;
+	let listOptions = "",
+		timeStep = timeMin;
 
-		while ( timeStep.getTime() <= timeMax.getTime() ) {
-			listOptions = `
+	while ( timeStep.getTime() <= timeMax.getTime() ) {
+		listOptions = `
 ${listOptions}
 <option value="${( timeStep.toLocaleTimeString( "en-GB" ) )}">`;
-			timeStep.setSeconds( timeStep.getSeconds() + inpStep );
-		}
+		timeStep.setSeconds( timeStep.getSeconds() + inpStep );
+	}
 
-		inp.setAttribute( "list", timeListId );
-		timeList.innerHTML = listOptions;
-		document.querySelector( "body" ).appendChild( timeList );
-	};
+	inp.setAttribute( "list", timeListId );
+	timeList.innerHTML = listOptions;
+	document.querySelector( "body" ).appendChild( timeList );
+};
 
+( d => {
 	d.addEventListener( "focusin", event => {
 		const tgt = event.target.closest( "input[type=time][min][max][step]" );
 
