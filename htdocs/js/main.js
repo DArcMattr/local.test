a2fc99fb60993fabf88975676dee75c384349cbd
David Arceneaux <david@davidthemachine.org>
2023-12-16 14:11:14 -0600

    :user-invalid has wide adoption, remove workaround

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -3,30 +3,13 @@ import "./modules/detailsAnimation.js";
 import "./modules/labeledClass.js";
 import "./modules/searchSelects.js";
 
-/**
- * A substitute for ':user-invalid', which is only supported in Firefox ATM as
- * ::-moz-ui-invalid
- */
 ((d) => {
+	// apply expando max size to side menu,
+	// TODO: recompute when page dimensions change
     const menuGroups = d.querySelectorAll("#left [role='navigation'] dd");
 
     menuGroups.forEach((el) => {
         el.style.setProperty("--flex-basis", `${el.scrollHeight}px`);
-    });
-
-    d.addEventListener("focusout", (event) => {
-        const sels = [
-            "input:not([type=checkbox]):not([type=radio])",
-            "select",
-            "textarea"
-        ];
-        const inputEl = event.target.closest(sels.join(","));
-        const theClass = "was-focused";
-
-        if (inputEl !== null &&
-            !inputEl.classList.contains(theClass)) {
-            inputEl.classList.add(theClass);
-        }
     });
 
     d.addEventListener("focusin", (event) => {
