81a51a4d2dff84db8f76195aa9d46482307daa33
David Arceneaux <david@davidthemachine.org>
2026-03-07 16:34:11 -0600

    Move over to a native CSS means of animating <details>

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,17 +1,8 @@
 import timeInputFallback from "./modules/timeInputFallback.js";
-import "./modules/detailsAnimation.js";
 import "./modules/labeledClass.js";
 import "./modules/searchSelects.js";
 
 ((d) => {
-	// apply expando max size to side menu,
-	// TODO: recompute when page dimensions change
-    const menuGroups = d.querySelectorAll("#left [role='navigation'] dd");
-
-    menuGroups.forEach((el) => {
-        el.style.setProperty("--flex-basis", `${el.scrollHeight}px`);
-    });
-
     d.addEventListener("focusin", (event) => {
         const tgt = event.target.closest("input[type=time][min][max][step]");
 
